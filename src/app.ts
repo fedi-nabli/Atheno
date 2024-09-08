72913ae226c4d036a12adc9d30b0d0af130c4197
fedi-nabli <fedinabli@gmail.com>
2024-09-08 20:49:48 +0100

    Initialized express server

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import express, { Request, Response } from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 
@@ -5,3 +6,13 @@ import connectDB from './config/db'
 
 dotenv.config()
 connectDB()
+
+const app = express()
+app.use(express.json())
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('API is running...')
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server running on port ${PORT} in ${process.env.NODE_ENV} mode`))
