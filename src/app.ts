6822bf1919b79bbbd6ade4ab324ad960700ee181
fedi-nabli <fedinabli@gmail.com>
2024-09-08 21:07:15 +0100

    Removed necessary packages

--- src/app.ts
+++ src/app.ts
@@ -19,4 +19,4 @@ app.use(notFound)
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`Server running on port ${PORT} in ${process.env.NODE_ENV} mode`))
+app.listen(PORT, () => console.log(colors.yellow.bold(`Server running on port ${PORT} in ${process.env.NODE_ENV} mode`)))
