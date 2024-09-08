5cebdcc575675fa8af59ec45a81b5baee3a9da66
fedi-nabli <fedinabli@gmail.com>
2024-09-08 20:58:30 +0100

    Added error moddleware

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 
 import connectDB from './config/db'
+import { notFound, errorHandler } from './middleware/errorMiddleware'
 
 dotenv.config()
 connectDB()
@@ -13,6 +14,9 @@ app.use(express.json())
 app.get('/', (req: Request, res: Response) => {
   res.send('API is running...')
 })
+
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server running on port ${PORT} in ${process.env.NODE_ENV} mode`))
---
+++ src/middleware/errorMiddleware.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from 'express'
+
+const notFound = (req: Request, res: Response, next: NextFunction) => {
+  const error: Error = new Error(`Not Found - ${req.originalUrl}`)
+  res.status(404)
+  next(error)
+}
+
+const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+  const statusCode: number = res.statusCode === 200 ? 500 : res.statusCode
+  res.status(statusCode)
+  res.send({
+    message: err.message,
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack
+  })
+}
+
+export {
+  notFound,
+  errorHandler
+}
